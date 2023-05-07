06434ce9699a695a768243d0ab9365d3ed3da413
stainy077 <stainy077@yandex.ru>
2023-05-07 20:24:31 +0700

    deploy v4

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -67,7 +67,7 @@ DATABASES = {
         'ENGINE': os.environ.get('DB_ENGINE', default='django.db.backends.postgresql'),
         'NAME': os.environ.get('DB_NAME', default='foodgram_data'),
         'USER': os.environ.get('DB_USER', default='postgres'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', default='password'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', default='postgres'),
         'HOST': os.environ.get('DB_HOST', default='db'),
         'PORT': os.environ.get('DB_PORT', default='5432')
     }
