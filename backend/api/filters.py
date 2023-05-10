42a7b0e9c59fb4b2811561f6318dd908c0d8301b
stainy077 <stainy077@yandex.ru>
2023-05-11 06:44:20 +0700

    deploy 028

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,6 +1,6 @@
 import django_filters as filters
 
-from recipes.models import Ingredient, Recipe, Tag
+from recipes.models import Ingredient, Recipe
 
 
 class RecipeFilter(filters.FilterSet):
--- backend/api/views_recipes.py
+++ backend/api/views_recipes.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import permissions, viewsets, status, filters
+from rest_framework import permissions, viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
